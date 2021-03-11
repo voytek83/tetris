f6e424ffca3e88cb0f725b1e25690d965fc6cf58
Wojciech Wysocki <voytek83@gmail.com>
2021-03-11 16:47:24 +0100

    droping piece

--- src/com/wojtek/Tetris.java
+++ src/com/wojtek/Tetris.java
@@ -2,15 +2,78 @@ package com.wojtek;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Random;
+
 
 public class Tetris extends JPanel {
 
     public static int pW;
-    int rotation = 0;
+    static int rotation = 0;
+    private Timer timer = new Timer(1000, evt -> dropPiece());
+    private final Point[][][] Tetraminos = {
+
+            {
+                    {new Point(0, 1), new Point(1, 1), new Point(2, 1), new Point(3, 1)},
+                    {new Point(1, 0), new Point(1, 1), new Point(1, 2), new Point(1, 3)},
+                    {new Point(0, 1), new Point(1, 1), new Point(2, 1), new Point(3, 1)},
+                    {new Point(1, 0), new Point(1, 1), new Point(1, 2), new Point(1, 3)}
+            },
+
+            {
+                    {new Point(0, 1), new Point(1, 1), new Point(2, 1), new Point(2, 0)},
+                    {new Point(1, 0), new Point(1, 1), new Point(1, 2), new Point(2, 2)},
+                    {new Point(0, 1), new Point(1, 1), new Point(2, 1), new Point(0, 2)},
+                    {new Point(1, 0), new Point(1, 1), new Point(1, 2), new Point(0, 0)}
+            },
+
+            {
+                    {new Point(0, 1), new Point(1, 1), new Point(2, 1), new Point(2, 2)},
+                    {new Point(1, 0), new Point(1, 1), new Point(1, 2), new Point(0, 2)},
+                    {new Point(0, 1), new Point(1, 1), new Point(2, 1), new Point(0, 0)},
+                    {new Point(1, 0), new Point(1, 1), new Point(1, 2), new Point(2, 0)}
+            },
+
+            {
+                    {new Point(0, 0), new Point(0, 1), new Point(1, 0), new Point(1, 1)},
+                    {new Point(0, 0), new Point(0, 1), new Point(1, 0), new Point(1, 1)},
+                    {new Point(0, 0), new Point(0, 1), new Point(1, 0), new Point(1, 1)},
+                    {new Point(0, 0), new Point(0, 1), new Point(1, 0), new Point(1, 1)}
+            },
+
+            {
+                    {new Point(1, 0), new Point(2, 0), new Point(0, 1), new Point(1, 1)},
+                    {new Point(0, 0), new Point(0, 1), new Point(1, 1), new Point(1, 2)},
+                    {new Point(1, 0), new Point(2, 0), new Point(0, 1), new Point(1, 1)},
+                    {new Point(0, 0), new Point(0, 1), new Point(1, 1), new Point(1, 2)}
+            },
+
+            {
+                    {new Point(1, 0), new Point(0, 1), new Point(1, 1), new Point(2, 1)},
+                    {new Point(1, 0), new Point(0, 1), new Point(1, 1), new Point(1, 2)},
+                    {new Point(0, 1), new Point(1, 1), new Point(2, 1), new Point(1, 2)},
+                    {new Point(1, 0), new Point(1, 1), new Point(2, 1), new Point(1, 2)}
+            },
+
+            {
+                    {new Point(0, 0), new Point(1, 0), new Point(1, 1), new Point(2, 1)},
+                    {new Point(1, 0), new Point(0, 1), new Point(1, 1), new Point(0, 2)},
+                    {new Point(0, 0), new Point(1, 0), new Point(1, 1), new Point(2, 1)},
+                    {new Point(1, 0), new Point(0, 1), new Point(1, 1), new Point(0, 2)}
+            }
+    };
+    public Point piecePlace;
     private int[][] tablica;
-    private Point origin;
+    private Integer currentPiece;
 
     public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+
+    private static void createAndShowGUI() {
         JFrame frame = new JFrame("Tetris");
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         pW = screenSize.height / 30;
@@ -21,28 +84,41 @@ public class Tetris extends JPanel {
         frame.setSize(frameWidth, frameHeight);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
-
-        final Tetris tetris = new Tetris();
-        tetris.zerowanieTablicy();
+        Tetris tetris = new Tetris();
         frame.add(tetris);
-        tetris.nowyKlocek();
-
-        new Thread() {
-            @Override
-            public void run() {
-                while (true) {
-                    try {
-                        Thread.sleep(1000);
-
-                        tetris.spadniecie();
-                    } catch (InterruptedException e) {
-                    }
-                }
-            }
-        }.start();
+        tetris.zerowanieTablicy();
 
 
     }
+
+    private void move(int i) {
+        if (collisionTest(piecePlace.x + i, piecePlace.y, rotation)) {
+            piecePlace.x += i;
+        }
+        repaint();
+
+    }
+
+
+    private void dropPiece() {
+        //if (collisionTest(piecePlace.x, piecePlace.y + 1, rotation)) {
+        for (Point p : Tetraminos[currentPiece][rotation]) {
+            tablica[p.x + piecePlace.x][p.y + piecePlace.y] = 0;
+        }
+        piecePlace.y += 1;
+
+        for (Point p : Tetraminos[currentPiece][rotation]) {
+            tablica[p.x + piecePlace.x][p.y + piecePlace.y] = 5;
+        }
+
+        // } else {
+        //     fixToWell();
+        // }
+
+        repaint();
+
+    }
+
 
     private void zerowanieTablicy() {
         tablica = new int[12][22];
@@ -60,6 +136,10 @@ public class Tetris extends JPanel {
             tablica[0][ij] = 1;
             tablica[11][ij] = 1;
         }
+
+        newPiece();
+        timer.start();
+
     }
 
     @Override
@@ -68,70 +148,127 @@ public class Tetris extends JPanel {
         for (int i = 0; i < 12; i++) {
             for (int j = 0; j < 22; j++) {
                 switch (tablica[i][j]) {
-                    case 0:
+                    case 0 -> {
                         g.setColor(Color.GRAY);
                         g.fillRect(i * pW, j * pW, pW, pW);
-                        break;
-                    case 1:
+                    }
+                    case 1 -> {
                         g.setColor(Color.DARK_GRAY);
                         g.fillRect(i * pW, j * pW, pW, pW);
-                        break;
-                    case 2:
+                    }
+                    case 2 -> {
                         g.setColor(Color.GREEN);
                         g.fillRect(i * pW, j * pW, pW, pW);
-                        break;
-                    case 3:
+                    }
+                    case 3 -> {
                         g.setColor(Color.YELLOW);
                         g.fillRect(i * pW, j * pW, pW, pW);
-                        break;
-                    case 4:
+                    }
+                    case 4 -> {
                         g.setColor(Color.RED);
                         g.fillRect(i * pW, j * pW, pW, pW);
-                        break;
-                    case 5:
+                    }
+                    case 5 -> {
                         g.setColor(Color.ORANGE);
                         g.fillRect(i * pW, j * pW, pW, pW);
-                        break;
-                    case 6:
+                    }
+                    case 6 -> {
                         g.setColor(Color.CYAN);
                         g.fillRect(i * pW, j * pW, pW, pW);
-                        break;
-                    case 7:
+                    }
+                    case 7 -> {
                         g.setColor(Color.MAGENTA);
                         g.fillRect(i * pW, j * pW, pW, pW);
-                        break;
-                    case 8:
+                    }
+                    case 8 -> {
                         g.setColor(Color.BLUE);
                         g.fillRect(i * pW, j * pW, pW, pW);
-                        break;
+                    }
+
+
                 }
+
+            }
+        }
+        drawPiece();
+
+    }
+
+    public void newPiece() {
+        piecePlace = new Point(4, 2);
+        rotation = 0;
+        Random generator = new Random();
+        currentPiece = generator.nextInt(6);
+
+    }
+
+    private void drawPiece() {
+        for (Point p : Tetraminos[currentPiece][rotation]) {
+            tablica[p.x + piecePlace.x][p.y + piecePlace.y] = 5;
+        }
+        repaint();
+    }
+
+    private boolean collisionTest(int x, int y, int rotation) {
+        for (Point p : Tetraminos[currentPiece][rotation]) {
+            if (tablica[p.x + x][p.y + y] == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void rotate(int i) {
+        int newRotation = (rotation + i) % 4;
+        if (newRotation < 0) {
+            newRotation = 3;
+        }
+        if (collisionTest(piecePlace.x, piecePlace.y, newRotation)) {
+            rotation = newRotation;
+        }
+        repaint();
+    }
+
+    public void fixToWell() {
+        for (Point p : Tetraminos[currentPiece][rotation]) {
+            tablica[piecePlace.x + p.x][piecePlace.y + p.y] = 2;
+        }
+        clearRows();
+        newPiece();
+    }
+
+    public void deleteRow(int row) {
+        for (int j = row - 1; j > 0; j--) {
+            for (int i = 1; i < 11; i++) {
+                tablica[i][j + 1] = tablica[i][j];
             }
         }
     }
 
-    private void nowyKlocek() {
-        origin = new Point(5, 2);
+    public void clearRows() {
+        boolean gap;
+
+
+        for (int j = 21; j > 0; j--) {
+            gap = false;
+            for (int i = 1; i < 11; i++) {
+                if (tablica[i][j] == 1) {
+                    gap = true;
+                    break;
+                }
+            }
+            if (!gap) {
+                deleteRow(j);
+                j += 1;
+
+            }
+        }
 
 
     }
-
-    private void spadniecie() {
-
-        origin.y += 1;
-        repaint();
-    }
-
-    private void rysowanieKlocka(klocki, rotation) {
-
-    }
-
-    private enum rotation {
-        1,2,3,4;
-    }
-
-    private enum klocki {
-        kwKloc, lKloc, odwLKloc, iKloc, zKloc, odwZKloc, tKloc;
-    }
-
 
 }
+
+
+
+
